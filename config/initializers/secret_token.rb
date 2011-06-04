3d7042214411599a5af5a2281ac50c04536d6ce5
Tim Mandelbaum <tim.mandelbaum@haw-hamburg.de>
2011-06-04 09:59:48 +0200

    Erster Kommentar

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Graphen::Application.config.secret_token = 'b8f93844596db9c439f0d0cb28f562676d11e3f06f85968fe898bf19b49ca8bdff9bdea6d424ed94a47a029b5af2c6bd8b54743297a3ede5e9510289be95a488'
